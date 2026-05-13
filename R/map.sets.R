d44535a889c8f0b77e21141dd1e2098a5cfb651d
jakeelement <74512300+jakeelement@users.noreply.github.com>
2026-05-13 14:32:34 -0300

    Use maps basemap with optional MapBox satellite tiles

--- R/map.sets.R
+++ R/map.sets.R
@@ -1,5 +1,5 @@
 #' @title map.sets
-#' @import dplyr RSQLite sf rnaturalearth
+#' @import dplyr RSQLite sf maps png
 #' @description Opens SET_INFO from a trip .db file and plots set coordinates as points.
 #' @export
 map.sets <- function(choose.trip = FALSE,
@@ -58,17 +58,45 @@ map.sets <- function(choose.trip = FALSE,
       remove = FALSE
     )
 
-    world_map <- rnaturalearth::ne_countries(scale = "large", returnclass = "sf")
-
     bbox <- sf::st_bbox(set_sf)
     lon_pad <- max((bbox["xmax"] - bbox["xmin"]) * 0.35, 0.3)
     lat_pad <- max((bbox["ymax"] - bbox["ymin"]) * 0.35, 0.3)
     xlim <- c(bbox["xmin"] - lon_pad, bbox["xmax"] + lon_pad)
     ylim <- c(bbox["ymin"] - lat_pad, bbox["ymax"] + lat_pad)
 
+
+    mapbox_token <- if(exists("mapbox.token", envir = .GlobalEnv)) get("mapbox.token", envir = .GlobalEnv) else NULL
+
+    if(!is.null(mapbox_token) && nzchar(mapbox_token)) {
+      mapbox_url <- paste0(
+        "https://api.mapbox.com/styles/v1/mapbox/satellite-v9/static/",
+        xlim[1], ",", ylim[1], ",", xlim[2], ",", ylim[2],
+        "/900x700?access_token=", mapbox_token
+      )
+
+      map_img_file <- tempfile(fileext = ".png")
+      downloaded <- FALSE
+      try(download.file(mapbox_url, map_img_file, mode = "wb", quiet = TRUE), silent = TRUE)
+      if(file.exists(map_img_file) && file.info(map_img_file)$size > 0){
+        downloaded <- TRUE
+      }
+
+      if(downloaded){
+        map_img <- png::readPNG(map_img_file)
+        plot(NA, xlim = xlim, ylim = ylim, xlab = "Longitude", ylab = "Latitude", axes = TRUE, asp = 1)
+        rasterImage(map_img, xlim[1], ylim[1], xlim[2], ylim[2])
+      } else {
+        warning("MapBox tile download failed; falling back to maps basemap.")
+        world_map <- sf::st_as_sf(maps::map("world", plot = FALSE, fill = TRUE))
+        plot(sf::st_geometry(world_map), col = "antiquewhite", border = "grey55", xlim = xlim, ylim = ylim, axes = TRUE)
+      }
+    } else {
+      world_map <- sf::st_as_sf(maps::map("world", plot = FALSE, fill = TRUE))
+      plot(sf::st_geometry(world_map), col = "antiquewhite", border = "grey55", xlim = xlim, ylim = ylim, axes = TRUE)
+    }
+
     graticule <- sf::st_graticule(set_sf, lon = pretty(xlim, 6), lat = pretty(ylim, 6))
 
-    plot(sf::st_geometry(world_map), col = "antiquewhite", border = "grey55", xlim = xlim, ylim = ylim, axes = TRUE)
     plot(sf::st_geometry(graticule), add = TRUE, col = "grey85", lty = 3)
     plot(sf::st_geometry(set_sf), add = TRUE, pch = 19, col = "blue")
     box()
